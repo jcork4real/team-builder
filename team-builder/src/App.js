bfc00d08d6fd0d0e9b761281fce54385400bb5b1
Jarone McCorkle <jaroneemccorkle@gmail.com>
2020-06-10 18:21:55 -0700

    almost mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Step 1 : Set Up State
+// Import the useState hook and set up state to keep your team members list.
+
+import React, {useState} from  'react';
+
+
 function App() {
+
+  // Step 1 : Set Up State
+// Import the useState hook and set up state to keep your team members list.
+
+  const [teamMembers, setTeamMembers] = useState([
+    // Give the state variable you just declared a default value. You will need to keep track of a 
+    // list of team members and each team member will have several key/value pairs associated with them.
+    {
+     id: 0,
+     name: 'Jcork 4real',
+     role: 'Ultimate Cool Guy'
+    },
+    {
+     id: 1,
+     name: 'Stork James',
+     role: 'Semi Ultimate Cool Guy'
+    },
+    {
+     id: 2,
+     name: 'Hunter Hurst Helmsley',
+     role: 'Average Cool Guy'
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Render your list of team members. */}
     </div>
   );
 }
